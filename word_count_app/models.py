594e3fbcad8e10e03d7587b7e1467ba673cb3668
Leticia Pillar Lisboa <leticiapillar@gmail.com>
2023-09-03 12:59:58 -0300

    Define word count model Register word count model admin Generate word count model migrations

--- word_count_app/models.py
+++ word_count_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class WordCountModel(models.Model):
+    text = models.TextField()
+    total_words = models.IntegerField()
+    
+    def __str__(self):
+        return (
+            f"text={self.text[:30]}...\n"
+            f"total_words={self.total_words}"
+        )
